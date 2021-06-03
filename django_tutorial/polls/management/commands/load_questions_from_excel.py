a41481af4127dbf354de77c958e77c4b6eb7e2c7
Seth Massarsky <smassarsky@findaway.com>
2021-06-03 09:43:16 -0400

    added atomic transaction and error handling to load_questions_from_excel

--- django_tutorial/polls/management/commands/load_questions_from_excel.py
+++ django_tutorial/polls/management/commands/load_questions_from_excel.py
@@ -1,8 +1,9 @@
 from django.utils import timezone
-
+from django.db import DatabaseError, transaction
 from django.core.management.base import BaseCommand
 from polls.models import Question
 from openpyxl import load_workbook
+from openpyxl.utils.exceptions import InvalidFileException
 
 
 class Command(BaseCommand):
@@ -12,21 +13,30 @@ class Command(BaseCommand):
         parser.add_argument('path', nargs='+', type=str)
 
     def handle(self, *args, **options):
-        print(options['path'])
-
-        wb = load_workbook(filename=options['path'][0])
+        try:
+            wb = load_workbook(filename=options['path'][0])
+        except InvalidFileException:
+            print('Invalid File.')
+            return
+        except FileNotFoundError:
+            print('File not found.')
+            return
 
         ws = wb.active
 
         row = 2
 
-        while ws.cell(column=1, row=row).value:
-            question = Question(question_text=ws.cell(column=1, row=row).value, pub_date=timezone.now())
-            question.save()
-            col = 2
-            while ws.cell(column=col, row=row).value:
-                question.choice_set.create(choice_text=ws.cell(column=col, row=row).value)
-                col += 1
-            row += 1
+        try:
+            with transaction.atomic():
+                while ws.cell(column=1, row=row).value:
+                    question = Question(question_text=ws.cell(column=1, row=row).value, pub_date=timezone.now())
+                    question.save()
+                    col = 2
+                    while ws.cell(column=col, row=row).value:
+                        question.choice_set.create(choice_text=ws.cell(column=col, row=row).value)
+                        col += 1
+                    row += 1
+        except DatabaseError:
+            print('Error creating questions.')
 
         print('Done creating questions.')
